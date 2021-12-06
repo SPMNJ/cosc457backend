2494f8d7c675c0ae5da1725133f74f027420e877
Sean McGuire <16692089+SPMNJ@users.noreply.github.com>
2021-12-06 12:13:24 -0500

    Forgot to update this

--- src/main/java/dev/teamswy/backend/BackendApplication.java
+++ src/main/java/dev/teamswy/backend/BackendApplication.java
@@ -23,16 +23,16 @@ public class BackendApplication {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
-	@Bean
-	public CommandLineRunner demo(IFraternityHQRepository repository) {
-		return args -> {
-			FraternityHQ hq = new FraternityHQ();
-			hq.setLetters("ΦΚΨ");
-			hq.setName("Phi Kappa Psi Fraternity");
-			hq.setDate(LocalDate.of(1852, 2, 19));
-			hq.setAddress("Laurel Hall; 5395 Emerson Way; Indianapolis, IN 46226; USA");
-			hq.setChapters(0);
-			repository.save(hq);
-		};
-	}		
+	// @Bean
+	// public CommandLineRunner demo(IFraternityHQRepository repository) {
+	// 	return args -> {
+	// 		FraternityHQ hq = new FraternityHQ();
+	// 		hq.setLetters("ΦΚΨ");
+	// 		hq.setName("Phi Kappa Psi Fraternity");
+	// 		hq.setDate(LocalDate.of(1852, 2, 19));
+	// 		hq.setAddress("Laurel Hall; 5395 Emerson Way; Indianapolis, IN 46226; USA");
+	// 		hq.setChapters(0);
+	// 		repository.save(hq);
+	// 	};
+	// }		
 }
